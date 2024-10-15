f50f663ec80891f3fc569daa6597a34ef86e1bf3
emir-olgun <emirolgun@gmail.com>
2024-10-15 15:33:19 +0300

    tests updated

--- src/backend/sso.test.ts
+++ src/backend/sso.test.ts
@@ -1,5 +1,5 @@
 import { Sso } from "./sso";
-import { verifyWalletAddress, metadataReader } from "./utils/utils";
+import { verifyWalletAddress, metadataReader, convertHexToBech32 } from "./utils/utils";
 import { SsoOptions, SsoResult } from "./types/types";
 import { error } from "console";
 
@@ -11,6 +11,9 @@ const mockVerifyWalletAddress = verifyWalletAddress as jest.MockedFunction<
 >;
 const mockMetadataReader = metadataReader as jest.MockedFunction<
   typeof metadataReader
+>;
+const mockHexToBech32 = convertHexToBech32 as jest.MockedFunction<
+  typeof convertHexToBech32
 >;
 
 describe("Sso", () => {
@@ -31,26 +34,25 @@ describe("Sso", () => {
         amount: 1,
       },
       issuerOption: "issuer",
-      platformUniqueIdentifier: "unique",
+      platformUniqueIdentifiers: ["unique"],
       usageCount: 2,
+      lastUsage: "2024-10-01",
     };
     mockVerifyWalletAddress.mockReturnValue(true);
     mockMetadataReader.mockResolvedValue([
       {
-        sso: {
-          version: "0.1.0",
-          uniqueIdentifier: "unique",
-          issuer: "issuer",
-          issuanceDate: "2022-01-01",
-          expirationDate: "2024-12-31",
-          isTransferable: 1,
-          tiedWallet: "wallet",
-          isMaxUsageEnabled: 1,
-          maxUsage: 10,
-          isInactivityEnabled: 1,
-          inactivityPeriod: 30,
-          role: "role",
-        },
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "issuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2024-12-31",
+        isTransferable: 1,
+        tiedWallet: "wallet",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod: "30d",
+        role: "role",
       },
       true,
     ]);
@@ -83,26 +85,24 @@ describe("Sso", () => {
         amount: 1,
       },
       issuerOption: "issuer",
-      platformUniqueIdentifier: "unique",
+      platformUniqueIdentifiers: ["unique"],
       usageCount: 2,
     };
     mockVerifyWalletAddress.mockReturnValue(true);
     mockMetadataReader.mockResolvedValue([
       {
-        sso: {
-          version: "0.1.0",
-          uniqueIdentifier: "unique",
-          issuer: "issuer",
-          issuanceDate: "2022-01-01",
-          expirationDate: "2022-12-31",
-          isTransferable: 1,
-          tiedWallet: "wallet",
-          isMaxUsageEnabled: 1,
-          maxUsage: 10,
-          isInactivityEnabled: 1,
-          inactivityPeriod: 30,
-          role: "role",
-        },
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "issuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2022-12-31",
+        isTransferable: 1,
+        tiedWallet: "wallet",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod: "30d",
+        role: "role",
       },
       true,
     ]);
@@ -135,26 +135,24 @@ describe("Sso", () => {
         amount: 1,
       },
       issuerOption: "issuer",
-      platformUniqueIdentifier: "unique",
+      platformUniqueIdentifiers: ["unique"],
       usageCount: 2,
     };
     mockVerifyWalletAddress.mockReturnValue(true);
     mockMetadataReader.mockResolvedValue([
       {
-        sso: {
-          version: "0.1.0",
-          uniqueIdentifier: "unique",
-          issuer: "wrongIssuer",
-          issuanceDate: "2022-01-01",
-          expirationDate: "2024-12-31",
-          isTransferable: 1,
-          tiedWallet: "wallet",
-          isMaxUsageEnabled: 1,
-          maxUsage: 10,
-          isInactivityEnabled: 1,
-          inactivityPeriod: 30,
-          role: "role",
-        },
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "wrongIssuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2024-12-31",
+        isTransferable: 1,
+        tiedWallet: "wallet",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod:"30d",
+        role: "role",
       },
       true,
     ]);
@@ -187,7 +185,7 @@ describe("Sso", () => {
         amount: 1,
       },
       issuerOption: "issuer",
-      platformUniqueIdentifier: "unique",
+      platformUniqueIdentifiers: ["unique"],
       usageCount: 2,
     };
     mockVerifyWalletAddress.mockReturnValue(true);
@@ -221,29 +219,29 @@ describe("Sso", () => {
         amount: 1,
       },
       issuerOption: "Issuer",
-      platformUniqueIdentifier: "unique",
+      platformUniqueIdentifiers: ["unique"],
       usageCount: 2,
+      lastUsage: "2024-10-01",
     };
+    mockHexToBech32.mockReturnValue("stake1uxyz");
     mockVerifyWalletAddress.mockReturnValue(true);
     mockMetadataReader.mockResolvedValue([
-        {
-          sso: {
-            version: "0.1.0",
-            uniqueIdentifier: "unique",
-            issuer: "Issuer",
-            issuanceDate: "2022-01-01",
-            expirationDate: "2024-12-31",
-            isTransferable: 0,
-            tiedWallet: "stake1uxyz",
-            isMaxUsageEnabled: 1,
-            maxUsage: 10,
-            isInactivityEnabled: 1,
-            inactivityPeriod: 30,
-            role: "role",
-          },
-        },
-        true,
-      ]);
+      {
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "Issuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2024-12-31",
+        isTransferable: 0,
+        tiedWallet: "stake1uxyz",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod: "30d",
+        role: "role",
+      },
+      true,
+    ]);
 
     const result: SsoResult = await Sso(options);
 
@@ -273,29 +271,27 @@ describe("Sso", () => {
         amount: 1,
       },
       issuerOption: "Issuer",
-      platformUniqueIdentifier: "unique",
+      platformUniqueIdentifiers: ["unique"],
       usageCount: 2,
     };
     mockVerifyWalletAddress.mockReturnValue(true);
     mockMetadataReader.mockResolvedValue([
-        {
-          sso: {
-            version: "0.1.0",
-            uniqueIdentifier: "unique",
-            issuer: "Issuer",
-            issuanceDate: "2022-01-01",
-            expirationDate: "2024-12-31",
-            isTransferable: 0,
-            tiedWallet: "stake2uxyz",
-            isMaxUsageEnabled: 1,
-            maxUsage: 10,
-            isInactivityEnabled: 1,
-            inactivityPeriod: 30,
-            role: "role",
-          },
-        },
-        true,
-      ]);
+      {
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "Issuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2024-12-31",
+        isTransferable: 0,
+        tiedWallet: "stake2uxyz",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod: "30d",
+        role: "role",
+      },
+      true,
+    ]);
 
     const result: SsoResult = await Sso(options);
 
@@ -325,35 +321,241 @@ describe("Sso", () => {
         amount: 1,
       },
       issuerOption: "Issuer",
-      platformUniqueIdentifier: "unique",
+      platformUniqueIdentifiers: ["unique"],
       usageCount: 10,
     };
     mockVerifyWalletAddress.mockReturnValue(true);
     mockMetadataReader.mockResolvedValue([
-        {
-          sso: {
-            version: "0.1.0",
-            uniqueIdentifier: "unique",
-            issuer: "Issuer",
-            issuanceDate: "2022-01-01",
-            expirationDate: "2024-12-31",
-            isTransferable: 0,
-            tiedWallet: "stake1uxyz",
-            isMaxUsageEnabled: 1,
-            maxUsage: 10,
-            isInactivityEnabled: 1,
-            inactivityPeriod: 30,
-            role: "role",
-          },
-        },
-        true,
-      ]);
+      {
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "Issuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2024-12-31",
+        isTransferable: 0,
+        tiedWallet: "stake1uxyz",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod: "30d",
+        role: "role",
+      },
+      true,
+    ]);
 
     const result: SsoResult = await Sso(options);
 
     expect(result).toEqual({
       success: false,
       error: "Maximum usage reached",
+    });
+    expect(mockVerifyWalletAddress).toHaveBeenCalledWith(
+      options.signature,
+      options.key,
+      options.nonce,
+      options.stakeAddress,
+      options.walletNetwork
+    );
+  });
+
+  test("should not validate the metadata sso due to expired inactivity in days", async () => {
+    const options: SsoOptions = {
+      stakeAddress: "stake1uxyz",
+      signature: "signature",
+      key: "key",
+      nonce: "nonce",
+      walletNetwork: 1, // walletNetwork is a number
+      asset: {
+        policyID: "policy",
+        assetName: "asset",
+        amount: 1,
+      },
+      issuerOption: "Issuer",
+      platformUniqueIdentifiers: ["unique"],
+      usageCount: 2,
+      lastUsage: "2024-09-01",
+    };
+    mockHexToBech32.mockReturnValue("stake1uxyz");
+    mockVerifyWalletAddress.mockReturnValue(true);
+    mockMetadataReader.mockResolvedValue([
+      {
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "Issuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2024-12-31",
+        isTransferable: 0,
+        tiedWallet: "stake1uxyz",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod: "30d",
+        role: "role",
+      },
+      true,
+    ]);
+
+    const result: SsoResult = await Sso(options);
+
+    expect(result).toEqual({
+      success: false,
+      error: "Inactivity period exceeded",
+    });
+    expect(mockVerifyWalletAddress).toHaveBeenCalledWith(
+      options.signature,
+      options.key,
+      options.nonce,
+      options.stakeAddress,
+      options.walletNetwork
+    );
+  });
+
+  test("should not validate the metadata sso due to expired inactivity in months", async () => {
+    const options: SsoOptions = {
+      stakeAddress: "stake1uxyz",
+      signature: "signature",
+      key: "key",
+      nonce: "nonce",
+      walletNetwork: 1, // walletNetwork is a number
+      asset: {
+        policyID: "policy",
+        assetName: "asset",
+        amount: 1,
+      },
+      issuerOption: "Issuer",
+      platformUniqueIdentifiers: ["unique"],
+      usageCount: 2,
+      lastUsage: "2024-01-01",
+    };
+    mockHexToBech32.mockReturnValue("stake1uxyz");
+    mockVerifyWalletAddress.mockReturnValue(true);
+    mockMetadataReader.mockResolvedValue([
+      {
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "Issuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2024-12-31",
+        isTransferable: 0,
+        tiedWallet: "stake1uxyz",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod: "3m",
+        role: "role",
+      },
+      true,
+    ]);
+
+    const result: SsoResult = await Sso(options);
+
+    expect(result).toEqual({
+      success: false,
+      error: "Inactivity period exceeded",
+    });
+    expect(mockVerifyWalletAddress).toHaveBeenCalledWith(
+      options.signature,
+      options.key,
+      options.nonce,
+      options.stakeAddress,
+      options.walletNetwork
+    );
+  });
+
+  test("should validate the metadata sso due to expired inactivity in years", async () => {
+    const options: SsoOptions = {
+      stakeAddress: "stake1uxyz",
+      signature: "signature",
+      key: "key",
+      nonce: "nonce",
+      walletNetwork: 1, // walletNetwork is a number
+      asset: {
+        policyID: "policy",
+        assetName: "asset",
+        amount: 1,
+      },
+      issuerOption: "Issuer",
+      platformUniqueIdentifiers: ["unique"],
+      usageCount: 2,
+      lastUsage: "2024-01-01",
+    };
+    mockHexToBech32.mockReturnValue("stake1uxyz");
+    mockVerifyWalletAddress.mockReturnValue(true);
+    mockMetadataReader.mockResolvedValue([
+      {
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "Issuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2024-12-31",
+        isTransferable: 0,
+        tiedWallet: "stake1uxyz",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod: "3y",
+        role: "role",
+      },
+      true,
+    ]);
+
+    const result: SsoResult = await Sso(options);
+
+    expect(result).toEqual({
+      success: true,
+      roles: "role",
+    });
+    expect(mockVerifyWalletAddress).toHaveBeenCalledWith(
+      options.signature,
+      options.key,
+      options.nonce,
+      options.stakeAddress,
+      options.walletNetwork
+    );
+  });
+
+  test("should not validate the metadata sso due to expired inactivity in years", async () => {
+    const options: SsoOptions = {
+      stakeAddress: "stake1uxyz",
+      signature: "signature",
+      key: "key",
+      nonce: "nonce",
+      walletNetwork: 1, // walletNetwork is a number
+      asset: {
+        policyID: "policy",
+        assetName: "asset",
+        amount: 1,
+      },
+      issuerOption: "Issuer",
+      platformUniqueIdentifiers: ["unique"],
+      usageCount: 2,
+      lastUsage: "2019-01-01",
+    };
+    mockHexToBech32.mockReturnValue("stake1uxyz");
+    mockVerifyWalletAddress.mockReturnValue(true);
+    mockMetadataReader.mockResolvedValue([
+      {
+        version: "0.1.0",
+        uniqueIdentifier: "unique",
+        issuer: "Issuer",
+        issuanceDate: "2022-01-01",
+        expirationDate: "2024-12-31",
+        isTransferable: 0,
+        tiedWallet: "stake1uxyz",
+        isMaxUsageEnabled: 1,
+        maxUsage: 10,
+        isInactivityEnabled: 1,
+        inactivityPeriod: "3y",
+        role: "role",
+      },
+      true,
+    ]);
+
+    const result: SsoResult = await Sso(options);
+
+    expect(result).toEqual({
+      success: false,
+      error: "Inactivity period exceeded",
     });
     expect(mockVerifyWalletAddress).toHaveBeenCalledWith(
       options.signature,
